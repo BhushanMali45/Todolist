c30c0f1ac46563b76e1725a58475283393db52ec
BhushanMali45 <112244238+BhushanMali45@users.noreply.github.com>
2023-01-28 23:22:31 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+import ToList from './component/todoreact/todo';
+
+const App = () => {
+  return (
+    <div>
+      
+      <ToList/>
+  
+
+    </div>
+  )
+};
+
+export default App;
